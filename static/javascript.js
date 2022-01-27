4fd4f7db7ce8ffcaa89d3a708b20ea13d233f7f6
Jinwoo Kim <jinwoo.kim@yale.edu>
2022-01-27 16:50:45 -0500

    commit again

--- static/javascript.js
+++ static/javascript.js
@@ -11,13 +11,16 @@ document.addEventListener('DOMContentLoaded', function() {
         let html = await response.text();
         let new_html = html.substring(html.indexOf("years") + 7, html.indexOf("btn btn-primary") - 76);
         let new_html_2 = html.substring(html.indexOf("levels") + 17, html.indexOf("tourneys") - 100);
-        if (level.innerHTML.trim() != new_html_2.trim()) {
+        console.log(level.innerHTML.trim(), level.innerHTML.trim().length)
+        console.log(new_html_2.trim(), new_html_2.trim().length)
+        if (level.innerHTML.trim().length - 6 != new_html_2.trim().length) {
             level.innerHTML = new_html_2;
         }
         year.innerHTML = new_html;
     });
     level.addEventListener('change', async function() {
         current_level = level.value;
+        console.log(level.value);
         let response = await fetch("/find?t=" + input.value.toLowerCase() + "&level=" + current_level);
         let html = await response.text();
         let new_html = html.substring(html.indexOf("years") + 7, html.indexOf("btn btn-primary") - 76);
